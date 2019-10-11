a5afa0089c305a8ce20628137171972eaca10b8a
bacila1990 <1smeh.ru@mail.ru>
2019-10-11 17:27:04 +0300

    task complited

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,35 +1,85 @@
 class TicTacToe {
-    constructor() {
+  constructor() {
+    this.playingField = [
+      [null, null, null],
+      [null, null, null],
+      [null, null, null]
+    ];
+    this.playerMove = "x";
+  }
 
+  getCurrentPlayerSymbol() {
+    return this.playerMove;
+  }
+
+  nextTurn(rowIndex, columnIndex) {
+    if (
+      this.playingField[rowIndex][columnIndex] === null &&
+      this.playerMove === "x"
+    ) {
+      this.playingField[rowIndex][columnIndex] = this.playerMove;
+      this.playerMove = "o";
+    } else if (
+      this.playingField[rowIndex][columnIndex] === null &&
+      this.playerMove === "o"
+    ) {
+      this.playingField[rowIndex][columnIndex] = this.playerMove;
+      this.playerMove = "x";
     }
+  }
 
-    getCurrentPlayerSymbol() {
-
+  isFinished() {
+    if (this.getWinner() || this.isDraw()) {
+      return true;
     }
+    return false;
+  }
 
-    nextTurn(rowIndex, columnIndex) {
-
+  getWinner() {
+    if (
+      (this.playingField[0][0] === "x" && this.playingField[0][1] === "x" && this.playingField[0][2] === "x")||
+      (this.playingField[1][0] === "x" && this.playingField[1][1] === "x" && this.playingField[1][2] === "x") ||
+      (this.playingField[2][0] === "x" && this.playingField[2][1] === "x" && this.playingField[2][2] === "x") ||
+      (this.playingField[0][0] === "x" && this.playingField[1][0] === "x" && this.playingField[2][0] === "x") ||
+      (this.playingField[0][1] === "x" && this.playingField[1][1] === "x" && this.playingField[2][1] === "x") ||
+      (this.playingField[0][2] === "x" && this.playingField[1][2] === "x" && this.playingField[2][2] === "x") ||
+      (this.playingField[0][0] === "x" && this.playingField[1][1] === "x" && this.playingField[2][2] === "x") ||
+      (this.playingField[0][2] === "x" && this.playingField[1][1] === "x" && this.playingField[2][0] === "x")) {
+      return "x";
+    } if (
+      (this.playingField[0][0] === "o" && this.playingField[0][1] === "o" && this.playingField[0][2] === "o") ||
+      (this.playingField[1][0] === "o" && this.playingField[1][1] === "o" && this.playingField[1][2] === "o") ||
+      (this.playingField[2][0] === "o" && this.playingField[2][1] === "o" && this.playingField[2][2] === "o") ||
+      (this.playingField[0][0] === "o" && this.playingField[1][0] === "o" && this.playingField[2][0] === "o") ||
+      (this.playingField[0][1] === "o" && this.playingField[1][1] === "o" && this.playingField[2][1] === "o") ||
+      (this.playingField[0][2] === "o" && this.playingField[1][2] === "o" && this.playingField[2][2] === "o") ||
+      (this.playingField[0][0] === "o" && this.playingField[1][1] === "o" && this.playingField[2][2] === "o") ||
+      (this.playingField[0][2] === "o" && this.playingField[1][1] === "o" && this.playingField[2][0] === "o")) {
+      return "o";
     }
+      return null;
+  }
 
-    isFinished() {
+  noMoreTurns() {
+    let result = true;
+    this.playingField.forEach(elements =>
+      elements.forEach(item => {
+        if (item === null) result = false;
+      })
+    );
+    return result;
+  }
 
+  isDraw() {
+    if (this.noMoreTurns() && !this.getWinner()) {
+      return true;
     }
+    return false;
+  }
 
-    getWinner() {
-
-    }
-
-    noMoreTurns() {
-
-    }
-
-    isDraw() {
-
-    }
-
-    getFieldValue(rowIndex, colIndex) {
-
-    }
+  getFieldValue(rowIndex, colIndex) {
+    return this.playingField[rowIndex][colIndex];
+  }
 }
 
 module.exports = TicTacToe;
